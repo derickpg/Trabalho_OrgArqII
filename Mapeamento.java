a40f622e3a16e40b2a5ddd669c6a1529c13b12b6
ViniciusAz <viniciussazevedo@gmail.com>
2018-11-05 13:22:43 -0200

    Mapeamento associativo parcial

--- Mapeamento.java
+++ Mapeamento.java
@@ -2,7 +2,7 @@
 public class Mapeamento {
 
     public void direto(int tamCache, int tamPalavra, int nPalavras, int[] ender) { //Mapeamento Direto
-        int blocoBits = Integer.toBinaryString(nPalavras).length() - 1;// Bloco = 4 , blocobits = 2 (00,01,10,11)   
+        int blocoBits = Integer.toBinaryString(nPalavras).length() - 1;// Bloco = 4 , blocobits = 2 (00,01,10,11)
         int bloco = tamPalavra * nPalavras; // Tamanho em Bits do bloco
         int l = 2; // Numero de linhas
         int tag = 32 - blocoBits - 1; // Numero de Bits da tag
@@ -20,7 +20,7 @@ public class Mapeamento {
             tag = tag - 1; // Tira 1 bit da tag e adiciona para a linha
             espacoUsado = aux; //Maior espaço usado é atualizado
         }
-        
+
         System.out.println("--------------------------------------------Informações Básicas----------------------------------------------------------------");
         System.out.println("Espaço efetivo usado -> " + espacoUsado + "bytes ,");
         System.out.println("A tag terá " + tag + " bits,");
@@ -28,15 +28,15 @@ public class Mapeamento {
         System.out.println("e o bloco terá " + nPalavras + " palavras por bloco.");
         System.out.println("--------------------------------------------FIM---------------------------------------------------------------------------------");
         System.out.println("");
-        
+
         String[][] cache = new String[l][nPalavras + 2]; // [Numero de Linhas][Tamanho da linha]
         cache = zeraCache(cache, l, nPalavras + 2); //Inicia a cache toda com Zeros
         int miss = 0; // Variavel que anota os miss's
         int hit = 0; // Variavel que anota os hit´s
         for (int i = 0; i < ender.length; i++) {
             String endereco = bits32(ender[i]); // Converto o endereço para o formato binario em 32bits
-            int li = Integer.toBinaryString(l).length() - 1; // Tamnho da linha em bits 
-           
+            int li = Integer.toBinaryString(l).length() - 1; // Tamnho da linha em bits
+
             /*
             // ----------- DEBUG ----------------------------------------------
             System.out.println("[Bits da Linha] = " + (Integer.toBinaryString(l).length() - 1));
@@ -46,9 +46,9 @@ public class Mapeamento {
             System.out.println(" [Posicao] = " + endereco.substring(tag + li, 32));
             // --------------- END DEBUG  ----------------------------------------------
             */
-            
-            
-            if ((cache[Integer.parseInt(endereco.substring(tag, tag + li), 2)][0]).equalsIgnoreCase("1")) { // SE o BV == 1        
+
+
+            if ((cache[Integer.parseInt(endereco.substring(tag, tag + li), 2)][0]).equalsIgnoreCase("1")) { // SE o BV == 1
                 System.out.println("HIT");
                 if ((cache[Integer.parseInt(endereco.substring(tag, tag + li), 2)][1]).equalsIgnoreCase(endereco.substring(0, tag))) {//Se a tag for igual
                     //System.out.println("HIT TAG " + "  - " + (Integer.parseInt(endereco.substring(tag + li, 32), 2) + 2) + " sub " + endereco.substring(tag + li, 32));
@@ -93,7 +93,7 @@ public class Mapeamento {
             cache[linha][j] = String.valueOf(enderAux);
             enderAux++;
         }
-        
+
         //DEBUG
         //[][][10][]
         //1  2  3 4
@@ -116,7 +116,7 @@ public class Mapeamento {
         return cache;
     }
 
-    
+
     //Método auxiliar onde converte um número inteiro para número binario com 32bits
     private String bits32(int endereco) {
         String bit = Integer.toBinaryString(endereco);
@@ -140,7 +140,91 @@ public class Mapeamento {
         }
     }
 
-    public void conjAssociativo() {
 
+  // Para o mapeamento conjunto associativo, deve-se poder informar o tamanho em
+  // bytes da cache, o número de palavras por bloco, o tamanho da o tamanho da palavra.
+  public void associativo(int tamCache, int tamPalavra, int nPalavras, int nWays, int[] entrada){
+    // endereco = [  tag  |  bitC  |  bitP  ]
+    // - Tamanho = num bits necessarios para representar o tamCache
+    //      ex: 1 GigaByte  = 30 bits, 8KB = 13 bits, 256MB = 28 bits
+    int endereco = Integer.toBinaryString(tamCache).length()-1;
+
+    // Numero de linhas = tamCache (em Bytes) / (tamPalavra*nPalavras / 8)
+    int nLinhas = tamCache / ((tamPalavra*nPalavras) / 8);
+    int nConjuntos = nLinhas / nWays; //conjuntos = nWays linhas por conjunto
+
+    // - Conteudo = TAG + bitC + bitP (num bits necessarios para representar nPalavras e nConjuntos)
+    int bitP = Integer.toBinaryString(nPalavras).length()-1; //ex palavras = 4 , bitP = 2 (00,01,10,11)
+    int bitC = Integer.toBinaryString(nConjuntos).length()-1; //ex 32 conjuntos, bitC = 5
+    int tag = endereco - bitP - bitC; // Numero de Bits da tag = endereco - bitPalavras - bitConjuntos
+
+    // bloco (de palavras) = tamPalavra x nPalavras
+    int bloco = tamPalavra * nPalavras; // Tamanho em Bits do bloco
+    // linha = DV (1 bit) + bloco
+    int linha = bloco + 1; // Tamanho da Linha em bits
+
+    // int memAssociativa = nLinhas/nConjuntos * tag;
+    int tamConjunto = nLinhas/nConjuntos;
+
+    int[][] conjAssociativo = new int[nConjuntos][tamConjunto];
+    int[][] cache = new int[nLinhas][1+nPalavras];
+    // ex: 1024 linhas, 4 conjuntos, 8 palavras
+    // conjAssociativo[4][256]
+    // cache[1024][9]
+    System.out.println("-------------------------");
+    System.out.println("Numero de linhas      : " + nLinhas);
+    System.out.println("Numero de vias        : " + nWays);
+    System.out.println("Conjuntos associativos: " + nConjuntos);
+    System.out.println("-------------------------");
+
+    // variaveis auxiliares
+    int miss = 0, hit = 0;
+    int entT, entC, entP; // valor de entrada Tag, Conj, Palavra
+    int conjToCache; // calcula a linha da cache a partir do conjunto
+
+    // Deverá ser disponibilizado ao menos dois algoritmos a serem escolhidos como
+    // política de substituição (LRU, LFU, Relógio, randômico, sequencial, ...).
+
+    // entrada 3149 = 110001001101 (ex: 8 palavras, 4 conjuntos)
+    // | 1100010 |   01 |  101 |
+    // |    entT | entC | entP |
+    for (int i = 0; i < entrada.length; i++) {
+      entP = entrada[i] & nPalavras-1;
+      entC = (entrada[i] >> bitP) & nConjuntos-1;
+      entT = (entrada[i] >> (bitC + bitP) & 0xFFFFFFFF);
+System.out.println("DEBUG> (" + entrada[i] + ") " + Integer.toBinaryString(entrada[i]) + " |" + Integer.toBinaryString(entT) + "|" + Integer.toBinaryString(entC) + "|" + Integer.toBinaryString(entP) + "|");
+      for (int j = 0; j < tamConjunto; j++) {
+        //verifica se a tag j, presente no conjAssociativo entC é igual a entT
+        if(conjAssociativo[entC][j] == entT){
+          //calcula a linha na memoria cache, EX: cache de 1024 linhas, 4 conj
+          //  no conjunto entC=2 (0-3), indice j=3 (0-255)
+          //  na memoria cache sera na linha 3 + 256*(2+1) = 3+768 = 771
+          conjToCache = j + tamConjunto*(entC+1);
+          //trabalha na memoria
+          if(cache[conjToCache][0] == 0) {
+System.out.println("DEBUG> deu merda, existe a tag mas nao tem DV");
+          } else /*hit?*/ {
+            // busca
+            if(entrada[i] == cache[conjToCache][entP]) {
+System.out.println("DEBUG> acerto mizeravi");
+System.out.println("Encontrei o valor " + entrada[i] + " na linha " + conjToCache + " posicao " + entP + " do bloco da cache. Cheguei aqui a partir da linha " + j + " do conjunto " + entC);
+            }
+          }
+
+          break; //deu hit para o for
+        }
+      } //end for
+      //chama o metodo de substituição
     }
+
+    // Como resultado desta escolha, deve-se informar o número de conjuntos
+    // associativos e o formato de interpretação do endereço (tag, conjunto, bloco).
+
+    System.out.println("-------------------------");
+    System.out.println("Numero de linhas      : " + nLinhas);
+    System.out.println("Numero de vias        : " + nWays);
+    System.out.println("Conjuntos associativos: " + nConjuntos);
+    System.out.println("-------------------------");
+
+  }
 }
--- main.java
+++ main.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class main {
     public static void main(String[] args) throws FileNotFoundException{
         Mapeamento m = new Mapeamento();
-         
-        
+
+
        /* int[][] cache = new int[2][4];
         cache[0][0] = 0;
         cache[1][0] = 1;
@@ -24,8 +24,41 @@ public class main {
             System.out.println(dados[i]);
         }
      */
-     int[] a = l.lerLinhas("txt.txt");
+      int[] a = l.lerLinhas("txt.txt");
              //m.direto(1024, 8, 4,a);
-             m.direto(2048, 8, 4,a);
+             // m.direto(2048, 8, 4,a);
+
+      //tamCache, tamPalav, nPalav, nWay, entrada
+      m.associativo(2048, 8, 4, 4, a);
+
+      int bin = 4096;
+      System.out.print("entrada "+ bin);
+      System.out.println(" ("+ Integer.toBinaryString(bin) + ")");
+      int pl = 8;
+      int cj = 4;
+      int bitPl = Integer.toBinaryString(pl).length() - 1;
+      int bitCj = Integer.toBinaryString(cj).length() - 1;
+
+//bin é a entrada em inteiro, da onde vai tirar a tag, linha/conjunto, palavra
+//CALCULO PARA ARMAZENAR OS BITS DA PALAVRA bin & pl-1
+//a ideia aqui eh usar uma mascara para obter somente os bits da palavra 00000000011111
+      System.out.print("Palv " + Integer.toBinaryString(bin & pl-1));
+      System.out.println(" (" + (bin & pl-1) + ")");
+
+//CALCULO PARA ARMAZENAR OS BITS DO CONJUNTO (OU DA LINHA) (bin >> bitPl) & cj-1
+// a ideia aqui eh deslocar >> o numero de bits da palavra e usar mascara 0000000011111
+      System.out.print("Conj " + Integer.toBinaryString((bin >> bitPl) & cj-1));
+      System.out.println(" ("+ ((bin >> bitPl) & cj-1) + ")");
+
+//CALCULO PARA ARMAZENAR OS BITS DA TAG (bin >> (bitCj + bitPl)) & 0xFFFFFFFF
+//ideia é deslocar >> o numero de bits que nao pertenca a tag (bits de pl + cj ou linha)
+// a mascara aqui e completa 111111111111111111111111 pq sobrou apenas a tag, entao queremos todos valores
+      System.out.print("Tag? " + Integer.toBinaryString((bin >> (bitCj + bitPl)) & 0xFFFFFFFF));
+      System.out.println(" (" + ((bin >> (bitCj + bitPl)) & 0xFFFFFFFF) + ")");
+
+
+
+      // System.out.println(Integer.toBinaryString((cj << bitPl) + pl-1));
+      // System.out.println((cj << bitPl) -1);
     }
 }
