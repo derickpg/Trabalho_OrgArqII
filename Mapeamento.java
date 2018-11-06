29995d754cbc9699f2b62c0989c8bea580e663de
Derick Garcez <garcezderick@gmail.com>
2018-11-06 17:30:28 -0200

    Atualizando o mapeamento
    Nova atualização no mapeamento direto, agora estou tratando como uma cache = matriz de inteiros.

--- Mapeamento.java
+++ Mapeamento.java
@@ -32,50 +32,46 @@ public class Mapeamento {
         System.out.println("--------------------------------------------FIM---------------------------------------------------------------------------------");
         System.out.println("");
 
-        String[][] cache = new String[l][nPalavras + 2]; // [Numero de Linhas][Tamanho da linha]
+        int[][] cache = new int[l][nPalavras + 2]; // [Numero de Linhas][Tamanho da linha]
         cache = zeraCache(cache, l, nPalavras + 2); //Inicia a cache toda com Zeros
         int miss = 0; // Variavel que anota os miss's
         int hit = 0; // Variavel que anota os hit´s
         for (int i = 0; i < ender.length; i++) {
             String endereco = bits32(ender[i]); // Converto o endereço para o formato binario em 32bits
             int li = Integer.toBinaryString(l).length() - 1; // Tamnho da linha em bits
+            int tagEnder = Integer.parseInt(endereco.substring(0, tag), 2);
+            int linhaEnder = Integer.parseInt(endereco.substring(tag, tag + li), 2);
 
-            /*
-            // ----------- DEBUG ----------------------------------------------
-            System.out.println("[Bits da Linha] = " + (Integer.toBinaryString(l).length() - 1));
-            System.out.println(li + " | " + endereco + " TAMANHO " + endereco.length() + "TAG " + tag);
-            System.out.println("[TAG] = " + endereco.substring(0, tag));
-            System.out.println(" [Linha] = " + endereco.substring(tag, tag + li) + " Linha Decimal " + Integer.parseInt(endereco.substring(tag, tag + li), 2));
-            System.out.println(" [Posicao] = " + endereco.substring(tag + li, 32));
-            // --------------- END DEBUG  ----------------------------------------------
-            */
-
-
-            if ((cache[Integer.parseInt(endereco.substring(tag, tag + li), 2)][0]).equalsIgnoreCase("1")) { // SE o BV == 1
-                System.out.println("HIT");
-                if ((cache[Integer.parseInt(endereco.substring(tag, tag + li), 2)][1]).equalsIgnoreCase(endereco.substring(0, tag))) {//Se a tag for igual
+            if ((cache[linhaEnder][0]) == 1) { // SE o BV == 1
+                System.out.println("HIT - TAG = " + cache[linhaEnder][1] + "| tagEnder = " + tagEnder + "| linha = " + linhaEnder);
+                if ((cache[linhaEnder][1]) == tagEnder) {//Se a tag for igual
                     //System.out.println("HIT TAG " + "  - " + (Integer.parseInt(endereco.substring(tag + li, 32), 2) + 2) + " sub " + endereco.substring(tag + li, 32));
                     int auxc = (Integer.parseInt(endereco.substring(tag + li, 32), 2) + 2); // Variavel auxiliar para guaradar a posicao do bloco que a palavra se encontra
-                    if ((cache[Integer.parseInt(endereco.substring(tag, tag + li), 2)][auxc]).equalsIgnoreCase(String.valueOf(ender[i]))) {// ve no bloco se ele está
+                    if ((cache[linhaEnder][auxc]) == ender[i]) {// ve no bloco se ele está
                         System.out.println("HIT EFETIVO");
                         printCache(cache, l, nPalavras+2);
                         //System.out.println("[Linha] " + Integer.parseInt(endereco.substring(tag, tag + li), 2) + " [Posição] " + Integer.parseInt(endereco.substring(tag + li, 32), 2));
                         hit++;
                     } else {
+                        System.out.println("HITMISS");
+                        miss++;
                         int enderAux = ender[i] - Integer.parseInt(endereco.substring(tag + li, 32), 2);
-                        cache = copiaBloco(Integer.parseInt(endereco.substring(tag, tag + li), 2), enderAux, cache, endereco.substring(0, tag), nPalavras);
+                        cache = copiaBloco(Integer.parseInt(endereco.substring(tag, tag + li), 2), enderAux, cache, tagEnder, nPalavras);
                         printCache(cache, l, nPalavras+2); // Metodo que printa a cache no final DEBUG
                     }
                 } else {
+                    System.out.println("HITMISS-2");
+                    miss++;
                     int enderAux = ender[i] - Integer.parseInt(endereco.substring(tag + li, 32), 2);
-                    cache = copiaBloco(Integer.parseInt(endereco.substring(tag, tag + li), 2), enderAux, cache, endereco.substring(0, tag), nPalavras);
+                    cache = copiaBloco(Integer.parseInt(endereco.substring(tag, tag + li), 2), enderAux, cache, tagEnder, nPalavras);
                     printCache(cache, l, nPalavras+2); // Metodo que printa a cache no final DEBUG
                 }
             } else {
+                System.out.println("HIT - TAG = " + cache[linhaEnder][1] + "| tagEnder = " + tagEnder + "| linha = " + linhaEnder);
                 System.out.println("MISS");
                 miss++;
                 int enderAux = ender[i] - Integer.parseInt(endereco.substring(tag + li, 32), 2); //Anota a primeira palavra que deve estar presente no bloco
-                cache = copiaBloco(Integer.parseInt(endereco.substring(tag, tag + li), 2), enderAux, cache, endereco.substring(0, tag), nPalavras);
+                cache = copiaBloco(Integer.parseInt(endereco.substring(tag, tag + li), 2), enderAux, cache, tagEnder, nPalavras);
                 printCache(cache, l, nPalavras+2); // Metodo que printa a cache no final DEBUG
             }
 
@@ -89,31 +85,21 @@ public class Mapeamento {
     //Método auxiliar onde realiza a cópia do bloco onde a palavra se encontra
     // Recebe como paramento a linha que ele vai copiar o bloco, o endereço que ele deve iniciar a copia do bloco
     // cache para ser atualizada, a tag para ser atualizada na cache, e a quantidade de palavras por bloco
-    private String[][] copiaBloco(int linha, int enderAux, String[][] cache, String tag, int nPalavras) {
-        cache[linha][0] = "1"; //Coloca o BV = 1
+    private int[][] copiaBloco(int linha, int enderAux, int[][] cache,int tag, int nPalavras) {
+        cache[linha][0] = 1; //Coloca o BV = 1
         cache[linha][1] = tag; //Colocas a TAG
         for (int j = 2; j < nPalavras + 2; j++) {
-            cache[linha][j] = String.valueOf(enderAux);
+            cache[linha][j] = enderAux;
             enderAux++;
         }
-
-        //DEBUG
-        //[][][10][]
-        //1  2  3 4
-        /*String debug = "|";
-        for (int j = 0; j < nPalavras + 2; j++) {
-            debug = debug + cache[linha][j] + "|";
-        }*/
-        //System.out.println("[LINHA] - " + linha + " [BV] - " + cache[linha][0]
-        //        + " [TAG] - " + cache[linha][1] + " [PALAVRAS] - " + debug);
         return cache;
     }
 
     //Método auxiliar para iniciar a cache sempre com zero em todas as posições
-    private String[][] zeraCache(String[][] cache, int l, int c) {
+    private int[][] zeraCache(int[][] cache, int l, int c) {
         for (int i = 0; i < l; i++) {
             for (int j = 0; j < c; j++) {
-                cache[i][j] = "0";
+                cache[i][j] = 0;
             }
         }
         return cache;
@@ -123,16 +109,16 @@ public class Mapeamento {
     //Método auxiliar onde converte um número inteiro para número binario com 32bits
     private String bits32(int endereco) {
         String bit = Integer.toBinaryString(endereco);
-        System.out.println("[Endereço Solicitado] = " + endereco + " [ele em bits] = " + bit);
         //System.out.println("[Tamanho do Endereço em bits]" + bit.length());
         int tam = 32 - bit.length();
         for (int i = 0; i < tam; i++) {
             bit = "0" + bit;
         }
+        System.out.println("[Endereço Solicitado] = " + endereco + " [ele em bits] = " + bit);
         return bit;
     }
 
-    public void printCache(String[][] cache, int l, int b) {
+    public void printCache(int[][] cache, int l, int b) {
         System.out.println("              BV |TAG|Bloco ");
         for (int i = 0; i < l; i++) {
             String aux = "";
--- main.java
+++ main.java
@@ -6,29 +6,14 @@ public class main {
     public static void main(String[] args) throws FileNotFoundException{
         Mapeamento m = new Mapeamento();
 
-
-       /* int[][] cache = new int[2][4];
-        cache[0][0] = 0;
-        cache[1][0] = 1;
-        cache[0][1] = 1;
-        cache[0][2] = 8;
-        cache[0][3] = 7;
-        cache[1][1] = 0;
-        cache[1][2] = 9;
-        cache[1][3] = 5;
-        m.printCache(cache, 2, 4);*/
-
        Leitor l = new Leitor();
-     /*  int[] dados = l.lerLinhas("txt.txt");
-        for (int i = 0; i < dados.length; i++) {
-            System.out.println(dados[i]);
-        }
-     */
-      int[] a = l.lerLinhas("txt.txt");
+       int[] a = l.lerLinhas("txt1.txt");
+       
+       
              //m.direto(1024, 8, 4,a);
              // m.direto(2048, 8, 4,a);
-
-      //tamCache, tamPalav, nPalav, nWay, entrada
+     //m.direto(1024, 8, 4,a); 
+ /*     //tamCache, tamPalav, nPalav, nWay, entrada
       m.associativo(512, 16, 8, 4, true, a);
 
       int bin = 4096;
@@ -60,5 +45,6 @@ public class main {
 
       // System.out.println(Integer.toBinaryString((cj << bitPl) + pl-1));
       // System.out.println((cj << bitPl) -1);
+*/
     }
 }
